baec227baa341bfad7a9a23be00c0df36b60e61d
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2022-10-31 09:03:05 +0100

    test014____tabwriter

---
+++ test014____tabwriter.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+)
+
+func main() {
+	fmt.Printf("example 14\n")
+	w := tabwriter.NewWriter(os.Stdout, 10, 0, 2, '.', tabwriter.Debug)
+
+	fmt.Fprintln(w, "some\trow\tA")
+	fmt.Fprintln(w, "some\trow\tB")
+	fmt.Fprintln(w, "some\trow\tC")
+	fmt.Fprintln(w, "some\trow\tD")
+
+	w.Flush()
+}
