100bf49eea18eba82ab7619803722917f5fbcda8
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2022-11-29 12:05:18 +0100

    gin - html form with validation + error messages + remembered form values

--- mymodule14-html-form-with-gin/cmd/main.go
+++ mymodule14-html-form-with-gin/cmd/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"log"
 	"mymodule14-html-form-with-gin/data"
 	"net/http"
 )
+
+func postUserRegistration(c *gin.Context) {
+
+	userRegistration := &data.UserRegistration{}
+
+	if err := c.ShouldBind(userRegistration); err != nil {
+		verrs := err.(validator.ValidationErrors)
+		// ---- convert validation errors to list of error messages (strings)
+		messages := make([]string, len(verrs))
+		log.Printf("verrs: %#v", len(verrs))
+		for i, verr := range verrs {
+			messages[i] = fmt.Sprintf("%s: %s", verr.Field() /*verr.Error()*/, "The field is required")
+		}
+		c.HTML(http.StatusBadRequest, "form.html.tmpl", gin.H{
+			"errors":           messages,
+			"userRegistration": userRegistration,
+		})
+		return
+	}
+	c.Redirect(http.StatusTemporaryRedirect, "/user-created")
+}
 
 func main() {
 	engine := gin.Default()
@@ -18,13 +42,13 @@ func main() {
 	})
 
 	// ---- handle html form
-	engine.POST("/handle-form", func(c *gin.Context) {
-		userRegistration := &data.UserRegistration{}
-		if err := c.Bind(userRegistration); err != nil {
-			c.Writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		c.String(http.StatusOK, "%#v", userRegistration)
+	engine.POST("/form", postUserRegistration)
+
+	// ---- after successful form submission the user gets redirected to here
+	engine.POST("/user-created", func(c *gin.Context) {
+		c.HTML(http.StatusCreated, "user-created.html.tmpl", gin.H{
+			"appName": "Fancy App",
+		})
 	})
 
 	engine.Run(":9090")
--- mymodule14-html-form-with-gin/data/user_registration.go
+++ mymodule14-html-form-with-gin/data/user_registration.go
@@ -2,15 +2,15 @@ package data
 
 // UserRegistration ist das zentrale Domänenobjekt des UserRegistrationService
 type UserRegistration struct {
-	//RequestId             string    // uuid of web request
-	//ID                    string    `bson:"_id, omitempty"`
-	Firstname string `form:"Firstname" binding:"required"`
-	Lastname  string `form:"Lastname" binding:"required"`
+	RequestId string `form:"-"` // uuid of web request
+	ID        string `form:"-" bson:"_id, omitempty"`
+	Firstname string `form:"firstname" binding:"required"`
+	Lastname  string `form:"lastname" binding:"required"`
 	//Email                 string    `form:"Email" binding:"required"`
 	//Firma                 string    `form:"Firma"`
 	//Schulungscode         string    `form:"Schulungscode" binding:"required"`
 	//Datum                 time.Time `form:"Datum" binding:"required" time_format:"2006-01-02"`
-	//DatenschutzAkzeptiert bool      `form:"DatenschutzAkzeptiert" binding:"required"`
+	TermsAccepted bool `form:"termsAccepted" binding:"required"`
 	//Confirmed             bool
 	////Adresse, etc nicht dargestellt
 }
